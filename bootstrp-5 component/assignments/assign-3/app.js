dbe315cbad6c7139671764389136464d766ece91
Kailas Puri <kailaspuri123@gmail.com>
2024-04-16 11:21:27 +0530

    first commit

---
+++ bootstrp-5 component/assignments/assign-3/app.js
@@ -0,0 +1,30 @@
+// Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+   return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// Toast 
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+var toastTrigger = document.getElementById('customBtn')
+var toastLiveExample = document.getElementById('customToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+
+// navbar
+
